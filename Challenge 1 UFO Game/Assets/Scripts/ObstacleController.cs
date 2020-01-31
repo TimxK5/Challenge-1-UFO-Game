8903b96e122fbcba4bfdd43b9af6facd1ae72cb8
TimxK5 <59641989+TimxK5@users.noreply.github.com>
2020-01-31 08:57:04 -0500

    Challenge 1 UFO Game Hiram Sun

---
+++ Challenge 1 UFO Game/Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public Vector2 a, b;
+    [Range(0, 1)]
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(a, b, Mathf.PingPong(Time.time * speed, 1)); 
+    }
+}
---
+++ Challenge 1 UFO Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public GameObject life1, life2, life3;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        life1.gameObject.SetActive(true);
+        life2.gameObject.SetActive(true);
+        life3.gameObject.SetActive(true);
+        winText.text = "";
+        SetCountText ();
+    }
+
+    void Update()
+    {
+        switch (lives)
+        {
+            case 3:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(true);
+                life3.gameObject.SetActive(true);
+                break;
+            case 2:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(true);
+                life3.gameObject.SetActive(false);
+                break;
+            case 1:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(false);
+                life3.gameObject.SetActive(false);
+                break;
+            case 0:
+                life1.gameObject.SetActive(false);
+                life2.gameObject.SetActive(false);
+                life3.gameObject.SetActive(false);
+                SetCountText();
+                Destroy(this);
+                break;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+
+        if (count == 12)
+        {
+            transform.position = new Vector2(50f, 50f);
+        }
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives--;
+            SetCountText();
+            /*
+            count--;
+            SetCountText();
+            */
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (lives <= 0)
+        {
+            winText.text = "Game Over! Game created by Hiram Sun!";
+        }
+        if (count >= 20)
+        {
+            winText.text = "You Win! Game created by Hiram Sun!";
+            Time.timeScale = 0;
+        }
+    }
+}
